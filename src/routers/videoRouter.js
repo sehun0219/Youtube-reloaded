ee80dddb15dc4b3a4991235d85feef2b03aa853d
Sehun <cloudnine0219@gmail.com>
2023-05-02 21:31:52 +1200

    add API GET and POST

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,11 +1,19 @@
 import express from "express";
-import { watch, edit, remove, upload } from "../controllers/videoController";
+import {
+  watch,
+  getEdit,
+  remove,
+  upload,
+  postEdit,
+} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 videoRouter.get("/upload", upload);
 videoRouter.get("/:id(\\d+)", watch);
-videoRouter.get("/:id(\\d+)/edit", edit);
+
+videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+
 videoRouter.get("/:id(\\d+)/delete", remove);
 
 export default videoRouter;
