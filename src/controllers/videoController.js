c0acea9bbdcfc3d3d8f79eab5c6043f4749ea7e7
Sehun <cloudnine0219@gmail.com>
2023-05-03 17:14:20 +1200

    add function to upload DB to mongoDB

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,12 +2,8 @@ import Video from "../models/Video";
 
 //Video.find({}, (err, videos) => {});
 export const home = async (req, res) => {
-  try {
-    const videos = await Video.find({});
-    return res.render("home", { pageTitle: "Home", videos });
-  } catch {
-    return res.render("server-Error");
-  }
+  const videos = await Video.find({});
+  return res.render("home", { pageTitle: "Home", videos });
 };
 
 export const watch = (req, res) => {
@@ -25,6 +21,26 @@ export const postEdit = (req, res) => {
   const { title } = req.body;
   return res.redirect(`/videos/${id}`);
 };
+
+export const getUpload = (req, res) => {
+  return res.render("upload", { pageTitle: `Uploading` });
+};
+export const postUpload = async (req, res) => {
+  const { title, description, hashtags } = req.body;
+  const video = new Video({
+    title,
+    description,
+    createdAt: Date.now(),
+    hashtags: hashtags.split(",").map((word) => `#${word}`),
+    meta: {
+      views: 0,
+      rating: 0,
+    },
+  });
+  const dbVideo = await video.save();
+  console.log(dbVideo);
+  return res.redirect("/");
+};
+
 export const remove = (req, res) => res.send("remove videos");
-export const upload = (req, res) => res.send("upload");
 export const search = (req, res) => res.send("search");
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -3,17 +3,16 @@ import {
   watch,
   getEdit,
   remove,
-  upload,
+  postUpload,
   postEdit,
+  getUpload,
 } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/upload", upload);
 videoRouter.get("/:id(\\d+)", watch);
-
+videoRouter.route("/upload").get(getUpload).post(postUpload);
 videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
-
 videoRouter.get("/:id(\\d+)/delete", remove);
 
 export default videoRouter;
